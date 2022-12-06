7a3b7d3da5fc0f854850eec82957758b3036b046
Nahom Habtemichael <nahomhabtemichael@gmail.com>
2022-12-06 14:28:08 +0300

    Time: 96 ms (50.77%), Space: 14.4 MB (86.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,17 @@
 class Solution:
-    def alphaNum(self, char):
-        return (("a" <= char.lower() <= "z") or ("0" <= char <= "9"))
+    # def alphaNum(self, char):
+    #     return (("a" <= char.lower() <= "z") or ("0" <= char <= "9"))
     def isPalindrome(self, s: str) -> bool:
-        
-        left_pointer = 0
-        right_pointer = len(s)-1
-        
-        while left_pointer < right_pointer:
-            
-            while left_pointer < right_pointer and not self.alphaNum(s[left_pointer]):
-                left_pointer += 1
-                
-            while right_pointer > left_pointer and not self.alphaNum(s[right_pointer]):
-                right_pointer -= 1
-                
-            if s[left_pointer].lower() != s[right_pointer].lower():
+    
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
                 return False
-            left_pointer += 1
-            right_pointer -= 1
+            i, j = i + 1, j - 1
         return True
         
  
